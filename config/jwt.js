05b7fbb269830cfad1dbc4405aa8cc530bca0bc8
akkarn1689 <akkarn1689@gmail.com>
2025-01-11 00:34:25 +0530

    complete assignment

--- config/jwt.js
+++
@@ -1,7 +0,0 @@
-// config/jwt.config.js
-require('dotenv').config();
-
-module.exports = {
-    JWT_SECRET: process.env.JWT_SECRET || 'your-secret-key',
-    JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '24h'
-};
